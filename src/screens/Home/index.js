282194e0da50214706d0aecc7481acf106ca62bb
Jeff Salisbury <salisbury.jeffery@gmail.com>
2021-05-10 21:27:10 -0400

    Calling action and consuming reducer on screen.

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './home.css'
 import { Content } from '../../components'
-function Home() {
+
+import { connect } from "react-redux";
+import { getBlogs } from "../../redux/actions/blogs.action";
+
+
+function Home({ blogs, getBlogs }) {
+    useEffect(() => getBlogs(), [getBlogs])
     return (
         <Content>
             <div>Home Comp</div>
+            {blogs.blogs.map(blog => (
+                <p key={blog.id}>{blog.title}</p>
+            ))}
         </Content>
     );
 }
 
-export default Home;
+export default connect(
+    ({ blogs }) => ({ blogs }),
+    { getBlogs }
+)(Home);
